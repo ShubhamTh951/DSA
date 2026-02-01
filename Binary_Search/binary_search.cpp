73ed8a6877bd96efdf0b61e5d66d5551c4d57c96
Shubham Thakur <shubhamth951@gmail.com>
2026-02-01 20:56:54 +0530

    Implement binary search

---
+++ Binary_Search/binary_search.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int binarySearch(const vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid; 
+            } else if (nums[mid] < target) {
+                left = mid + 1; 
+            } else {
+                right = mid - 1; 
+            }
+        }
+
+        return -1;
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> nums = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int target = 5;
+    int result = solution.binarySearch(nums, target);
+
+    if (result != -1) {
+        cout << "Target found at index: " << result << endl;
+    } else {
+        cout << "Target not found." << endl;
+    }
+
+    return 0;
+}

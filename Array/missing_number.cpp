34648d1d3f0900f10c70508033f13f95f2840241
Shubham Thakur <shubhamth951@gmail.com>
2026-02-04 21:28:48 +0530

    Implement missing number in an array

---
+++ Array/missing_number.cpp
@@ -0,0 +1,23 @@
+# include <iostream>
+# include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int expected_sum = n * (n + 1) / 2;
+        int actual_sum = 0;
+        for (int num : nums) {
+            actual_sum += num;
+        }
+        return expected_sum - actual_sum;
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> nums = {3, 0, 1};
+    cout << "Missing number: " << solution.missingNumber(nums) << endl; // Output: 2
+    return 0;
+}

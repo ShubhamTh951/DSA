2fe2ff0be04b5a1f66c49f999985192cd109951e
Shubham Thakur <shubhamth951@gmail.com>
2026-02-10 20:32:00 +0530

    Best time to buy and sell stocks

---
+++ Array/best_time_to_buy_and_sell_stock.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+        int mini = prices[0];
+        for (int i = 1; i < prices.size(); i++) {
+            int  cost = prices[i] - mini;
+            profit = max(profit, cost);
+            mini = min(mini, prices[i]);
+        }
+        return profit;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> prices = {7, 1, 5, 3, 6, 4};
+    cout << sol.maxProfit(prices) << endl; // Output: 5
+    return 0;
+}

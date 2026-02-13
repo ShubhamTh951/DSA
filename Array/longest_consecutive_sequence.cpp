ddd6bb7fac0bdae2a0fb21da6b5bb5438452900e
Shubham Thakur <shubhamth951@gmail.com>
2026-02-13 21:30:36 +0530

    optimal sol of longest consecutive sequence

--- Array/longest_consecutive_sequence.cpp
+++ Array/longest_consecutive_sequence.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <unordered_map>
+#include <unordered_set>
 #include <algorithm>
 using namespace std;
 
@@ -27,11 +27,34 @@ public:
         }
         return longest;
     }
+
+    int optimal(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) return 0;
+        int longest = 1;
+        unordered_set<int> st;
+        for (int i = 0; i < n; i++) {
+            st.insert(nums[i]);
+        }
+        for (auto it : st) {
+            if (st.find(it - 1) == st.end()) {
+                int cnt = 1;
+                int x = it;
+                while (st.find(x + 1) != st.end()) {
+                    cnt++;
+                    x = x + 1;
+                }
+                longest = max(longest, cnt);
+                            }
+        }
+        return longest;
+    }
 };
 
 int main() {
     Solution sol;
     vector<int> nums = {100, 4, 200, 1, 3, 2};
-    cout << sol.longestConsecutive(nums) << endl; // Output: 4
+    // cout << sol.longestConsecutive(nums) << endl; 
+    cout << sol.optimal(nums) << endl;
     return 0;
 }

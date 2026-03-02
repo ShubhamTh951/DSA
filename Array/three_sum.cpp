112adc923ce6725d9b96378b67cd6294fefe8512
Shubham Thakur <shubhamth951@gmail.com>
2026-03-02 19:30:49 +0530

    Implement optimal sol of 3 sum

--- Array/three_sum.cpp
+++ Array/three_sum.cpp
@@ -50,13 +50,42 @@ public:
         vector<vector<int>> ans(st.begin(), st.end());
         return ans;
     }
+
+    vector<vector<int>> twoPointerSolution(vector<int>& nums) {
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+            int j = i + 1;
+            int k = nums.size() - 1;
+            while (j<k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum < 0) {
+                    j++;
+                }
+                else if (sum > 0) {
+                    k--;
+                }
+                else {
+                    vector<int> temp = {nums[i], nums[j], nums[k]};
+                    ans.push_back(temp);
+                    j++;
+                    k--;
+                    while(j < k && nums[j] == nums[j-1]) j++;
+                    while(j < k && nums[k] == nums[k+1]) k--;
+                }
+            }
+        }
+        return ans;
+    }
 };
 
 int main() {
     Solution solution;
     vector<int> nums = {-1, 0, 1, 2, -1, -4};
     // vector<vector<int>> result = solution.threeSum(nums);
-    vector<vector<int>> result = solution.hashSetSolution(nums);
+    // vector<vector<int>> result = solution.hashSetSolution(nums);
+    vector<vector<int>> result = solution.twoPointerSolution(nums);
     cout << "Unique triplets that sum to zero:" << endl;
     for (const auto& triplet : result) {
         cout << "[";

c1bff506d462e60bb28c382a8887ac15eccbdaa6
Shubham Thakur <shubhamth951@gmail.com>
2026-03-02 19:13:10 +0530

    Implement brute force and better approach of 3 sum

---
+++ Array/three_sum.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_set>
+using namespace std;
+
+struct VectorHash {
+    size_t operator()(const vector<int>& v) const {
+        size_t hash = 0;
+        for (int num : v) {
+            hash ^= std::hash<int>()(num) + 0x9e3779b9 + (hash << 6) + (hash >> 2);
+        }
+        return hash;
+    }
+};
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        unordered_set<vector<int>, VectorHash> st;
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                for (int k = j + 1; k < nums.size(); k++) {
+                    if (nums[i] + nums[j] + nums[k] == 0) {
+                        vector<int> temp = {nums[i], nums[j], nums[k]};
+                        sort(temp.begin(), temp.end());
+                        st.insert(temp);
+                    }
+                }
+            }
+        }
+        vector<vector<int>> ans(st.begin(), st.end());
+        return ans;
+    }
+
+    vector<vector<int>> hashSetSolution(vector<int>& nums) {
+        unordered_set<vector<int>, VectorHash> st;
+        for (int i = 0; i < nums.size(); i++) {
+            unordered_set<int> seen;
+            for (int j = i + 1; j < nums.size(); j++) {
+                int third = -(nums[i] + nums[j]);
+                if (seen.find(third) != seen.end()) {
+                    vector<int>temp = {nums[i], nums[j], third};
+                    sort(temp.begin(), temp.end());
+                    st.insert(temp);
+                }
+                seen.insert(nums[j]);
+            }
+        }
+        vector<vector<int>> ans(st.begin(), st.end());
+        return ans;
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> nums = {-1, 0, 1, 2, -1, -4};
+    // vector<vector<int>> result = solution.threeSum(nums);
+    vector<vector<int>> result = solution.hashSetSolution(nums);
+    cout << "Unique triplets that sum to zero:" << endl;
+    for (const auto& triplet : result) {
+        cout << "[";
+        for (size_t i = 0; i < triplet.size(); i++) {
+            cout << triplet[i];
+            if (i < triplet.size() - 1) {
+                cout << ", ";
+            }
+        }
+        cout << "]" << endl;
+    }
+    return 0;
+}

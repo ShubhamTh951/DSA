779d78cb983f8f3c00ab8fc7a1ed1de3b62f576a
Shubham Thakur <shubhamth951@gmail.com>
2026-04-03 21:14:53 +0530

    Implement pallindrome of LL using fast and slow pointer approach

--- Linked_List/palindrome.cpp
+++ Linked_List/palindrome.cpp
@@ -11,6 +11,19 @@ struct ListNode {
 };
  
 class Solution {
+private:
+    ListNode* reverseLL(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while (curr != nullptr) {
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
 public:
     bool isPalindrome(ListNode* head) {
         stack<int> st;
@@ -29,6 +42,31 @@ public:
 
         return true;
     }
+
+    bool isPalindrome2(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) return true;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast->next != nullptr && fast->next->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode* newHead = reverseLL(slow->next);
+        ListNode* first = head;
+        ListNode* second = newHead;
+        while (second != nullptr) {
+            if (first->val != second->val) {
+                reverseLL(newHead);
+                return false;
+            }
+            first = first->next;
+            second = second->next;
+        }
+        reverseLL(newHead);
+        return true;
+    }
 };
 
 int main() {
@@ -41,6 +79,9 @@ int main() {
 
     bool result = solution.isPalindrome(head);
     cout << "Is the linked list a palindrome? " << (result ? "Yes" : "No") << endl;
+
+    bool result2 = solution.isPalindrome2(head);
+    cout << "Is the linked list a palindrome? " << (result2 ? "Yes" : "No") << endl;
 
     return 0;
 }

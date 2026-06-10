e63b71ad55019cf95a14c2269418a62c42b00888
Shubham Thakur <shubhamth951@gmail.com>
2026-06-10 20:34:41 +0530

    solve remove element problem using two pointers

---
+++ Array/remove_element.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.size(); j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+};  
+
+int main() {
+    Solution solution;
+    vector<int> nums = {3, 2, 2, 3};
+    int val = 3;
+    int newLength = solution.removeElement(nums, val);
+    cout << "New length: " << newLength << endl;
+    for (int i = 0; i < newLength; i++) {
+        cout << nums[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
